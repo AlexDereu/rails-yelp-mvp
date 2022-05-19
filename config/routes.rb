51ec5795c4d31fc5c017059a64cba365051764de
Alex Dereu <adereu08@gmail.com>
2022-05-19 08:46:02 +0200

    MEP

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,21 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "restaurants#index"
+
+  # get    "restaurants",          to: "restaurants#index"
+  # # Create
+  # get    "restaurants/new",      to: "restaurants#new",  as: :new_restaurant
+  # post   "restaurants",          to: "restaurants#create"
+  # # Read one - The `show` route needs to be *after* `new` route.
+  # get    "restaurants/:id",      to: "restaurants#show", as: :restaurant
+  # # Update
+  # # get    "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+  # # patch  "restaurants/:id",      to: "restaurants#update"
+  # # Delete
+  # # delete "restaurants/:id",      to: "restaurants#destroy"
+
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+  end
 end
